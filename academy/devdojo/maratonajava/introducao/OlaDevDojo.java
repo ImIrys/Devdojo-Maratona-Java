0548cca922a6c6bbd5a0e73413dd200023009afa
ImIrys <irys.lda@gmail.com>
2024-02-01 23:18:52 -0300

    Adicionando aula 9 - Comentários

--- academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -2,8 +2,23 @@
 
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Comentário <b>javadoc</b>: comentário oficial,
+ * usado para descrever a classe e o método
+ * Para descrever o que está acontecendo para pessoas que estão utilizando o meu código.
+ */
 public class OlaDevDojo {
     public static void main(String[] args) {
         System.out.println("Olá, pequena padauã.");
     }
 }
+
+// Comentário em 1 linha
+
+/*
+Comentário em
+múltiplas
+linhas
+ */
+
+// Os métodos públicos que tem na classe devem ser comentados em javadoc
