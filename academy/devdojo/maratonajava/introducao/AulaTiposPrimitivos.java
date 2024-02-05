49e7d35966467506189db3b14b9c984b5ce5b347
ImIrys <irys.lda@gmail.com>
2024-02-05 20:48:20 -0300

    Adicionando Aula - Tipos Primitivos - Parte 1

---
+++ academy/devdojo/maratonajava/introducao/AulaTiposPrimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class AulaTiposPrimitivos {
+    public static void main(String[] args) {
+        /* 8 tipos primitivos:
+         int, double, float, char, byte, short, long, boolean
+         */
+        // Nomenclatura de variáveis: int idadeDoPai, char nomeDaMae
+        int anoNascimento = 1992;
+        int anoAtual = 2024;
+        int idadeIrys = anoAtual - anoNascimento;
+            System.out.println("Minha idade atual é: " +idadeIrys+ " anos.");
+
+
+    }
+}
