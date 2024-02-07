3913258c62f35c2f2c2407048274f0cb7bf0f247
ImIrys <irys.lda@gmail.com>
2024-02-06 21:43:21 -0300

    [200~12 - Tipos primitivos pt 03 - casting

--- academy/devdojo/maratonajava/introducao/AulaTiposPrimitivos.java
+++ academy/devdojo/maratonajava/introducao/AulaTiposPrimitivos.java
@@ -9,12 +9,10 @@ public class AulaTiposPrimitivos {
          */
         // Nomenclatura de variáveis: int idadeDoPai, char nomeDaMae
 
-        int anoNascimento = 1992;
-        int anoAtual = 2024; // numérico = 4 bytes = -2,147,483,648 até 147,483,647
-        int idadeIrys = anoAtual - anoNascimento;
-        long numeroGrande = 15000; // numérico = 8 bytes = -9,223,372,036,854,775,808 até 9223372036854750000
-        double salarioDouble = 3500; // números decimais = 8 bytes = 1.7e-308 até 1.7e+038
-        float salarioFloat = 4500; // números decimais = 4 bytes = 3.4e-038 até 3.4e+038
+        int idadeIrys = (int) 320000000000L; // numérico = 4 bytes = -2,147,483,648 até 147,483,647
+        long numeroGrande = 15000L; // variável que guarda valor numérico = 8 bytes = -9,223,372,036,854,775,808 até 9223372036854750000 | Especificar que é un long: inserir L no final
+        double salarioDouble = 3500.0D; // variáveis que guardam valores decimais = 8 bytes = 1.7e-308 até 1.7e+038 | Especificar que é un float: inserir F no final
+        float salarioFloat = 4500.0F; // variáveis que guardam valores decimais = 4 bytes = 3.4e-038 até 3.4e+038  |  Especificar que é un double: inserir D no final
         byte idadeByte = 10; // numérico = 1 byte = -128 até 127
         short idadeShort = 10; // numérico = 2 bytes = -32768 até 32767
         boolean verdadeiro = true; // 1 bit = true ou false
@@ -22,7 +20,13 @@ public class AulaTiposPrimitivos {
         char caractereA = 73; // 2 bytes = traduz um valor decimal em caractere da tabela ASCII
         char caractereU = '\u0041'; // traduz um valor decimal em caractere da tabela UNICODE
 
-        System.out.println("Minha idade atual é: " +idadeIrys+ " anos.");
+        /*
+        Cast: Quando forçamos o JAVA a inserir o valor de uma variável dentro da outra.
+        Exemplo: int idade = (int) 10000000000L;
+        Não é uma boa prática.
+         */
+
+        System.out.println("A idade é " +idadeIrys+ " anos");
         System.out.println(caractereA);
         System.out.println(caractereU);
     }
