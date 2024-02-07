bc1edd8ae4b836ddb2c87f17bab641c3a576a230
ImIrys <irys.lda@gmail.com>
2024-02-06 23:01:03 -0300

    Adicionando aula - Tipos primitivos pt 05 - Exercício

--- src/br/com/devdojo/java/marathon/Aula03TiposPrimitivosExercicio.java
+++ src/br/com/devdojo/java/marathon/Aula03TiposPrimitivosExercicio.java
@@ -1,4 +1,24 @@
 package br.com.devdojo.java.marathon;
 
+/*
+PRATICANDO
+
+CRIE VARIÁVEIS PARA OS CAMPOS DESCRITOS ABAIXO ENTRE <>
+E IMPRIMA A SEGUINTE MENSAGEM:
+EU <NOME>, MORANDO NO ENDEREÇO <ENDEREÇO>,
+CONFIRMO QUE RECEBI O SALÁRIO <SALARIO>, NA DATA <DATA>.
+ */
+
 public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Irys Albuquerque";
+        String adress = "Rua Mem de Sá - Icaraí";
+        double salary = 5500;
+        String receivingDate = "13/05/2024";
+        //Guardar os valores dentro de uma variável  String
+        String report = "Eu " +name+ ", moradora do endereço " + adress+ ", \nconfirmo que recebi o salário de " +salary+ ", na data " +receivingDate;
+
+        System.out.println(report);
+        //System.out.println("Eu " +name+ ", moradora do endereço " + adress+ ", \nconfirmo que recebi o salário de " +salary+ ", na data " +receivingDate);
+    }
 }
