60e35020e3d6cd574e4c86e37428beec21b91b47
ImIrys <irys.lda@gmail.com>
2024-02-15 23:13:40 -0300

    Adicionando Aula 22 sobre Estruturas Condicionais - Parte 03 - Operador Ternário

---
+++ src/br/com/devdojo/java/marathon/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,12 @@
+package br.com.devdojo.java.marathon;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Doar se salário > 5000
+        double salario = 6000;
+        // (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? "Vou conseguir doar 500,00" : "Não posso doar nada.";
+
+        System.out.println(resultado);
+    }
+}
