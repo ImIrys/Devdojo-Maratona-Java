b7552f2592cf1f94b07d3fd458301c064cd4bad8
ImIrys <irys.lda@gmail.com>
2024-02-20 22:25:14 -0300

    Study: add lesson 27 - Estruturas de Repetição - PT 01 - Laços de repetição while, do while, for

---
+++ src/br/com/devdojo/java/marathon/Aula0EstruturasRepeticao01.java
@@ -0,0 +1,23 @@
+package br.com.devdojo.java.marathon;
+
+public class Aula0EstruturasRepeticao01 {
+    public static void main(String[] args) {
+        // WHILE
+        int count = 0;
+        while (count < 10){
+            System.out.println(count);
+           count += 1; // count = count + 1;
+        }
+
+        //DO-WHILE
+        count = 0;
+        do {
+            System.out.println("dentro de DO-WHILE " + ++count);
+        } while (count < 10);
+
+        //FOR
+        for (int i=0; i < 10; i++) {
+            System.out.println("For " +i);
+        }
+    }
+}
