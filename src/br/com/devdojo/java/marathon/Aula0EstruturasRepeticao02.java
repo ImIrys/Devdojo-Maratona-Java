171c3e618de6e4fea96ef6fae8b59709b0b597c5
ImIrys <irys.lda@gmail.com>
2024-02-20 23:00:40 -0300

    Study: add lesson 28 - Estruturas de Repetição PT 02 - Exercício For

---
+++ src/br/com/devdojo/java/marathon/Aula0EstruturasRepeticao02.java
@@ -0,0 +1,12 @@
+package br.com.devdojo.java.marathon;
+
+public class Aula0EstruturasRepeticao02 {
+    public static void main(String[] args) {
+        //Utilizando o FOR, imprima todos os números pares de 0 até 1000000.
+        for (int i = 0; i <= 1000000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
