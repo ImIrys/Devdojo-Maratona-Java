8163174ca567249f6de6ad80af142fc2f0f35aba
ImIrys <irys.lda@gmail.com>
2024-03-07 21:18:44 -0300

    Study: add lesson 31 - Estruturas de Repetição pt 05 - Continue

---
+++ src/br/com/devdojo/java/marathon/Aula06EstruturasRepeticao05.java
@@ -0,0 +1,16 @@
+package br.com.devdojo.java.marathon;
+
+public class Aula06EstruturasRepeticao05 {
+    // Dado o valor de um carro, descubra em quantas vezes pode ser parcelado.
+    // Condição - valor da parcela >= 1000
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = (int)valorTotal; parcela >= 1; parcela--) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000){
+                continue;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
