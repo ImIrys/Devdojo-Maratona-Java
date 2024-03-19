eef57083e6c93b74db7cbcaa0d9280912b4783f6
ImIrys <irys.lda@gmail.com>
2024-03-18 21:02:00 -0300

    Study: add lesson 32 - Arrays PT 01

---
+++ src/br/com/devdojo/java/marathon/Aula07Arrays01.java
@@ -0,0 +1,14 @@
+package br.com.devdojo.java.marathon;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        // Guardar 3 idades de pessoas
+        int[] idades = new int[3];
+        idades[0] = 31;
+        idades[1] = 32;
+        idades[2] = 30;
+
+        System.out.println(idades[0]);
+
+    }
+}
