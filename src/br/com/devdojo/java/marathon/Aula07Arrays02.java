1cc5d7a083caac634d02a8af6bfa0eff0df389ce
ImIrys <irys.lda@gmail.com>
2024-03-19 21:22:04 -0300

    Study: add lesson 33 - Arrays - PT 02

---
+++ src/br/com/devdojo/java/marathon/Aula07Arrays02.java
@@ -0,0 +1,22 @@
+package br.com.devdojo.java.marathon;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        System.out.println(idades[2]);
+
+        char[] idades2 = new char[3];
+        System.out.println(idades2[2]);
+
+        float[] idades3 = new float[3];
+        System.out.println(idades3[2]);
+
+        String[] nomes = new String[3];
+        System.out.println(nomes[2]);
+
+        boolean[] idades4 = new boolean[3];
+        System.out.println(idades4[2]);
+    }
+
+
+}
