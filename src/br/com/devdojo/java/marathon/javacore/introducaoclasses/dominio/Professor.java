6f31802bad8f74b789b8f012657008f0084fdb32
ImIrys <irys.lda@gmail.com>
2024-03-28 22:01:33 -0300

    Study: add lesson 41 - Orientação Objetos - Coesão

---
+++ src/br/com/devdojo/java/marathon/javacore/introducaoclasses/dominio/Professor.java
@@ -0,0 +1,7 @@
+package br.com.devdojo.java.marathon.javacore.introducaoclasses.dominio;
+
+public class Professor {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/br/com/devdojo/java/marathon/javacore/introducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package br.com.devdojo.java.marathon.javacore.introducaoclasses.test;
+
+import br.com.devdojo.java.marathon.javacore.introducaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Kakashi";
+        professor.idade = 35;
+        professor.sexo = 'M';
+
+        System.out.println("Nome: " + professor.nome + ", Idade: "+professor.idade + " anos, Sexo: "+professor.sexo);
+    }
+}
