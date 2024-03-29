bbb279713ef190b72dc8e778ae43ffc76a6419d4
ImIrys <irys.lda@gmail.com>
2024-03-28 23:07:45 -0300

    Study: add lesson 42 - Orientação Objetos - Exercício Classes

---
+++ src/br/com/devdojo/java/marathon/javacore/introducaoclasses/test/CarroText01.java
@@ -0,0 +1,26 @@
+package br.com.devdojo.java.marathon.javacore.introducaoclasses.test;
+
+import br.com.devdojo.java.marathon.javacore.introducaoclasses.dominio.Carro;
+
+public class CarroText01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Nissan March";
+        carro1.modelo = "SV 1.6";
+        carro1.ano = 2024;
+
+        carro2.nome = "Jeep Renegade";
+        carro2.modelo = "1.3 Turbo";
+        carro2.ano = 2024;
+
+        System.out.println("===================  1ª OPÇÃO: ===================");
+        System.out.println(carro1.nome + ", " +carro1.modelo + ", " +carro1.ano);
+        System.out.println("\n===================  2ª OPÇÃO: ===================");
+        System.out.println(carro2.nome + ", " +carro2.modelo + ", " +carro2.ano);
+
+
+    }
+
+}
