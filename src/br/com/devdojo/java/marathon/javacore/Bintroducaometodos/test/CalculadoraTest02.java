80ee2413b6d6d41d4e21a54342377945594a3540
ImIrys <irys.lda@gmail.com>
2024-03-29 23:52:35 -0300

    Study: add lesson 45 - Orientação Objetos - Métodos pt 02 - Parâmetros

---
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package br.com.devdojo.java.marathon.javacore.Bintroducaometodos.test;
+
+import br.com.devdojo.java.marathon.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 50);
+    }
+}
