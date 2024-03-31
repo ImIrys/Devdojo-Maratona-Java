dacdd8fd82cb7baa6a2b5fb30baaf6c811ea53eb
ImIrys <irys.lda@gmail.com>
2024-03-30 21:35:03 -0300

    Study: add lesson 47 - Orientação Objetos - Métodos pt 04 - Retorno pt 02

--- src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -3,23 +3,36 @@ package br.com.devdojo.java.marathon.javacore.Bintroducaometodos.dominio;
 public class Calculadora {
 
     public void somaDoisNumeros() {
-        System.out.println(10+10);
+        System.out.println(10 + 10);
     }
 
-    public void subtraiDoisNumeros(){
-        System.out.println(35-3);
+    public void subtraiDoisNumeros() {
+        System.out.println(35 - 3);
     }
 
-    public void multiplicaDoisNumeros(int num1, int num2){
+    public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
     }
 
-    public double divideDoisNumeros(double num1, double num2){
+    public double divideDoisNumeros(double num1, double num2) {
         if (num1 == 0 || num2 == 0) {
-            return 0;
+            return 0; // a partir daqui ele retorna para acima
         }
-        return num1/num2;
+        return num1 / num2; // linha não será executada
     }
 
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        } else
+            return num1 / num2;
+    }
 
+    public void imprimeDivisaoDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero.");
+        } else {
+            System.out.println(num1 / num2);
+        }
+    }
 }
--- src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,7 +5,10 @@ import br.com.devdojo.java.marathon.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double resultado = calculadora.divideDoisNumeros(2,3);
+        double resultado = calculadora.divideDoisNumeros(20,0);
         System.out.println(resultado);
+        System.out.println(calculadora.divideDoisNumeros02(20,0));
+        System.out.println("======================================");
+        calculadora.imprimeDivisaoDoisNumeros(20,0);
     }
 }
