a42ca2d9da87d771ab3f8ea5bbf7fb47ca1e2386
ImIrys <irys.lda@gmail.com>
2024-03-31 23:16:07 -0300

    Study: add lesson 50 - Orientação Objetos - Métodos pt 07 - Parâmetros tipo referência pt 02

--- src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -4,9 +4,14 @@ public class ImpressoraEstudante {
     public void imprime(Estudante estudante){
         System.out.println("====================================");
 
+        /*
+        estudante.nome = "Tanjiro"; // vai alterar o conteúdo do objeto nome.
+        */
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
+
+        estudante.nome = "Tanjiro"; // o objeto em memória que tem a referência para NOME vai recebr o valor de Tanjiro
 
     }
 
--- src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -22,6 +22,12 @@ public class EstudanteTest01 {
 
         impressora.imprime(estudante02);
 
+        System.out.println("########################");
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
+
+
+
     }
 
 }
