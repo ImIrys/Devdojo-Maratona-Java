89025bd5fedd3eabbecd310907b3a4b0b2048592
ImIrys <irys.lda@gmail.com>
2024-04-02 22:05:42 -0300

    Study: add lesson 51 - Orientação Objetos - Métodos pt 08 - Referência this

--- src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/Estudante.java
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -4,4 +4,11 @@ public class Estudante {
     public String nome;
     public int idade;
     public char sexo;
+
+    public void imprime(){ // como está dentro do objeto, não necessita de parâmetro, pois tem acesso a todos os atributos dentro do objeto.
+        System.out.println("==================================");
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+    }
 }
---
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,21 @@
+package br.com.devdojo.java.marathon.javacore.Bintroducaometodos.test;
+
+import br.com.devdojo.java.marathon.javacore.Bintroducaometodos.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        estudante01.nome = "Sakura";
+        estudante01.idade = 25;
+        estudante01.sexo = 'F';
+
+        estudante02.nome = "Naruto";
+        estudante02.idade = 23;
+        estudante02.sexo = 'M';
+
+        estudante01.imprime();
+        estudante02.imprime();
+    }
+}
