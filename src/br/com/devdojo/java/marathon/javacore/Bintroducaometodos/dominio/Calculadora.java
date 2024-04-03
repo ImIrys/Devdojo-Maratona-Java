d22742d299b5753c9d4807ee842073d9022b5d54
ImIrys <irys.lda@gmail.com>
2024-04-02 23:00:26 -0300

    Study: add lesson 52 - Orientação Objetos - Métodos pt 09 - Varargs

--- src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -44,5 +44,19 @@ public class Calculadora {
         System.out.println("Número 2: "+numero2);
     }
 
+    public void somaArray(int[] numeros){
+        int soma = 0;
+        for (int num : numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+    public void somaVarArgs(int ... numeros){
+        int soma = 0;
+        for (int num : numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
 
 }
---
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/CalculadoraTest05.java
@@ -0,0 +1,14 @@
+package br.com.devdojo.java.marathon.javacore.Bintroducaometodos.test;
+
+import br.com.devdojo.java.marathon.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest05 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int[] numeros = {1,2,3,4,5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(1,2,3,4,5);  //calculadora.somaVarArgs(numeros);
+    }
+
+
+}
