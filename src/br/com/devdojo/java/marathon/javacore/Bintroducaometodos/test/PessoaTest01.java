0c9fcba921197bdcd4a096369a35ba4ad8174a3e
ImIrys <irys.lda@gmail.com>
2024-04-15 21:45:16 -0300

    Study: add lesson 54 - Orientação Objetos - Modificador de acesso private, get e set pt 01

---
+++ src/br/com/devdojo/java/marathon/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -0,0 +1,15 @@
+package br.com.devdojo.java.marathon.javacore.Bintroducaometodos.test;
+
+import br.com.devdojo.java.marathon.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Ellora");
+        pessoa.setIdade(5);
+
+       // pessoa.imprime();
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
