b467dfae53fbb9bc344c262eebc4f26fd85d4091
ImIrys <irys.lda@gmail.com>
2024-04-23 01:08:20 -0300

    Study

---
+++ src/br/com/devdojo/java/marathon/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package br.com.devdojo.java.marathon.javacore.Dconstrutores.test;
+
+
+import br.com.devdojo.java.marathon.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Demon Slayer","TV",12, "Ação" );
+        anime.imprime();
+
+    }
+}
