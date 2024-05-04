82b80d2f4e23ca01aa685357a59e0bec36e16183
ImIrys <irys.lda@gmail.com>
2024-05-04 19:13:06 -0300

    Study: add lesson 59 - Orientação Objetos - Construtores pt 02 - Sobrecarga

---
+++ src/br/com/devdojo/java/marathon/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -0,0 +1,9 @@
+package br.com.devdojo.java.marathon.javacore.Eblocosinicializacao.test;
+
+import br.com.devdojo.java.marathon.javacore.Eblocosinicializacao.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+    }
+}
