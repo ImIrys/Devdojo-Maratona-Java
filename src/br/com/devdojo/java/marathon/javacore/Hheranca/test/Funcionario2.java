35acf6fe80ae75019907a73cc09377a211101ea9
ImIrys <irys.lda@gmail.com>
2024-05-21 07:35:14 -0300

    Study: add lesson 73 - Orientação Objetos - Herança pt 03 - protected

---
+++ src/br/com/devdojo/java/marathon/javacore/Hheranca/test/Funcionario2.java
@@ -0,0 +1,9 @@
+package br.com.devdojo.java.marathon.javacore.Hheranca.test;
+
+import br.com.devdojo.java.marathon.javacore.Hheranca.domain.Pessoa;
+
+public class Funcionario2 extends Pessoa {
+    public void imprime(){
+        this.nome = "sasa";
+    }
+}
