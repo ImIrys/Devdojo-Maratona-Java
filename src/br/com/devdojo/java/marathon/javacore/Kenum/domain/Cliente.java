ffadc35eb5624e8d02f4d64c9304ff62c2b940c3
ImIrys <irys.lda@gmail.com>
2024-06-10 08:08:07 -0300

    Study: add lesson 81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos and add lesson 82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- src/br/com/devdojo/java/marathon/javacore/Kenum/domain/Cliente.java
+++ src/br/com/devdojo/java/marathon/javacore/Kenum/domain/Cliente.java
@@ -3,10 +3,12 @@ package br.com.devdojo.java.marathon.javacore.Kenum.domain;
 public class Cliente {
     private String nome;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
 
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
@@ -14,22 +16,8 @@ public class Cliente {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
                 ", tipoCliente=" + tipoCliente +
+                ", tipoClienteInt=" + tipoCliente.VALOR +
+                ", tipoPagamento=" + tipoPagamento +
                 '}';
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public TipoCliente getTipoCliente() {
-        return tipoCliente;
-    }
-
-    public void setTipoCliente(TipoCliente tipoCliente) {
-        this.tipoCliente = tipoCliente;
     }
 }
--- src/br/com/devdojo/java/marathon/javacore/Kenum/test/ClienteTeste01.java
+++ src/br/com/devdojo/java/marathon/javacore/Kenum/test/ClienteTeste01.java
@@ -2,17 +2,20 @@ package br.com.devdojo.java.marathon.javacore.Kenum.test;
 
 import br.com.devdojo.java.marathon.javacore.Kenum.domain.Cliente;
 import br.com.devdojo.java.marathon.javacore.Kenum.domain.TipoCliente;
+import br.com.devdojo.java.marathon.javacore.Kenum.domain.TipoPagamento;
+
+import static br.com.devdojo.java.marathon.javacore.Kenum.domain.TipoPagamento.CREDITO;
+import static br.com.devdojo.java.marathon.javacore.Kenum.domain.TipoPagamento.DEBITO;
 
 public class ClienteTeste01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Irys", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente2 = new Cliente("Ellora", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tanjiro", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Naruto", TipoCliente.PESSOA_FISICA);
+        Cliente cliente1 = new Cliente("Irys", TipoCliente.PESSOA_JURIDICA, DEBITO);
+        Cliente cliente2 = new Cliente("Ellora", TipoCliente.PESSOA_FISICA, CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+        System.out.println(DEBITO.calcularDesconto(100));
+        System.out.println(CREDITO.calcularDesconto(100));
+
     }
 }
